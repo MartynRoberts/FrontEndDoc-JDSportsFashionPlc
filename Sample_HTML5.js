01afa992462208e82f326356d01bb6bd3135bb0d
Khaleel ibn Anwar Mughal <khaleelm@gmail.com>
2014-12-12 15:48:06 +0000

    Create Sample_HTML5.js

---
+++ Sample_HTML5.js
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<!--
+/***
+
+ * --- --- --- --- ---
+ * JD Sports Fashion plc
+ * IBM WebSphere Commerce Platform
+ * Khaleel Mughal
+ * --- --- --- --- ---
+ * #SAMPLEHTML5
+ * --- --- --- --- ---
+
+***/
+-->
+<head>
+  <meta charset="UTF-8">
+  <title>JD HTML5</title>
+</head>
+<body>
+  
+  <!--
+  // HEADER
+  // -- Uses h1 for SEO
+  -->
+  <header>
+    <h1><a href="/" title="JD Sports">JD Sports</a></h1>
+  </header>
+  
+</body>
+</html>

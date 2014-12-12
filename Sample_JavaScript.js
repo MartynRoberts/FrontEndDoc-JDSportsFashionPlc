d643b748976acb83777fe83830652baaad5ae6fa
Khaleel ibn Anwar Mughal <khaleelm@gmail.com>
2014-12-12 15:43:59 +0000

    Create Sample_JavaScript.js

---
+++ Sample_JavaScript.js
@@ -0,0 +1,32 @@
+/***
+
+ * --- --- --- --- ---
+ * JD Sports Fashion plc
+ * IBM WebSphere Commerce Platform
+ * Khaleel Mughal
+ * --- --- --- --- ---
+ * #SIMPLELOOP
+ * --- --- --- --- ---
+
+**/
+
+// START
+// -- Load Program
+
+  // CONFIGURE
+  // -- Array to iterate
+  var nameArray = [
+   { name: 'john', surname: 'smith'  },
+   { name: 'paul', surname: 'jones' },
+   { name: 'timi', surname: 'abel' },
+  ];  
+	
+  // LOOP
+  // -- Do the loop!
+  for (str of nameArray) {    
+   console.log( str.name );
+  }
+
+// END
+// -- EOF
+});
